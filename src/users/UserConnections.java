150f6f18e2c51668ed415f2cc469de94925160ba
fraser-stewart <nathaniel.tomsett@gmail.com>
2020-05-26 18:44:34 +0100

    Fixed player adding (except empty name)

--- src/users/UserConnections.java
+++ src/users/UserConnections.java
@@ -2,11 +2,14 @@ package users;
 
 import engine.CommandHandler;
 import engine.World;
+import entities.NPC;
 import entities.Player;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UserConnections extends Thread {
@@ -38,10 +41,50 @@ public class UserConnections extends Thread {
                     stream.initForConsole();
                 }
 
+
+
                 //TODO: hack-tastic
+                List<NPC> NPCList = world.getNPCList();
+                List<Player> PlayerList = new ArrayList<>();
+
+                boolean nameOk = false;
+               String username = "";
+                while (!nameOk){
+                   boolean foundNPC = false;
+
+
                 stream.printToUser("what is your name?");
-                String username = stream.readFromUser();
-                String playerName = username ;
+                 username = stream.readFromUser();
+                for (NPC i : NPCList){
+                    if (i.getName().equalsIgnoreCase(username)){
+                        stream.printToUser("this username is already taken");
+                         foundNPC = true;
+                    }
+
+
+                }
+                if (foundNPC){
+                    continue;
+
+                }
+                   Player playerExists = world.getPlayer(username);
+
+                if (playerExists != null) {
+                    stream.printToUser("username is taken");
+                    continue;
+                    }
+
+
+
+
+
+
+
+
+                        nameOk = true;
+
+                }
+                String playerName = username;
                 world.addPlayer(playerName, new Player(playerName));
                 CommandHandler processor = new CommandHandler(world, playerName, stream);
                 inputProcessors.put(playerName, processor);
